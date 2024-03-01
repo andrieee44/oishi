2c491671b553f46bb864b611e8f74a44379ca089
andrieee44 <andrieee44@gmail.com>
2024-03-01 21:31:32 +0800

    remove edit option, add key option

--- main.go
+++ main.go
@@ -5,18 +5,38 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
 
-func readFile(flag int) (*os.File, map[string]string) {
+func help() {
+	fmt.Fprintf(os.Stderr, `usage: %s { help | keys | copy <KEY> }
+
+oishi is a simple clipboard program using CSV
+
+  help - print this help message
+  keys - print all keys to standard output, '\n' will be escaped
+  copy - print the value of <KEY> to standard output
+
+add key-value CSV records to $XDG_DATA_HOME/oishi/oishi.csv
+add comments with '#'
+`, os.Args[0])
+}
+
+func die(format string, a ...any) {
+	fmt.Fprintf(os.Stderr, format, a...)
+	help()
+	os.Exit(1)
+}
+
+func readFile() [][]string {
 	var (
-		file      *os.File
-		dir       string
-		reader    *csv.Reader
-		recsSlice [][]string
-		recs      map[string]string
-		i         int
-		ok        bool
-		err       error
+		file   *os.File
+		dir    string
+		reader *csv.Reader
+		recs   [][]string
+		dup    map[string]bool
+		i      int
+		err    error
 	)
 
 	dir = os.Getenv("XDG_DATA_HOME")
@@ -35,7 +55,7 @@ func readFile(flag int) (*os.File, map[string]string) {
 		panic(err)
 	}
 
-	file, err = os.OpenFile(dir+"/oishi.csv", flag|os.O_CREATE, 0644)
+	file, err = os.OpenFile(dir+"/oishi.csv", os.O_RDONLY|os.O_CREATE, 0644)
 	if err != nil {
 		panic(err)
 	}
@@ -44,57 +64,38 @@ func readFile(flag int) (*os.File, map[string]string) {
 	reader.FieldsPerRecord = 2
 	reader.Comment = '#'
 
-	recsSlice, err = reader.ReadAll()
+	recs, err = reader.ReadAll()
 	if err != nil {
 		panic(err)
 	}
 
-	recs = make(map[string]string, len(recsSlice))
-	for i = range recsSlice {
-		_, ok = recs[recsSlice[i][0]]
-		if ok {
-			panic(fmt.Errorf("key %q is repeated", recsSlice[i][0]))
-		}
-
-		recs[recsSlice[i][0]] = recsSlice[i][1]
+	if len(recs) == 0 {
+		die("%s: oishi.csv is empty\n", os.Args[0])
 	}
-
-	return file, recs
-}
-
-func closeFile(file *os.File) {
-	var err error
 
 	err = file.Close()
 	if err != nil {
 		panic(err)
 	}
-}
 
-func help() {
-	fmt.Fprintf(os.Stderr, `usage: %s { help | copy <KEY> | add <KEY> <VALUE> }
+	dup = make(map[string]bool, len(recs))
+	for i = range recs {
+		if dup[recs[i][0]] {
+			panic(fmt.Errorf("key %q is repeated", recs[i][0]))
+		}
 
-%s is a simple clipboard program.
+		dup[recs[i][0]] = true
+	}
 
-  help - display this help message
-  copy - print the value of <KEY> to standard output
-  add  - set the value of <KEY> to <VALUE>
-`, os.Args[0], os.Args[0])
-}
-
-func die(format string, a ...any) {
-	fmt.Fprintf(os.Stderr, format, a...)
-	help()
-	os.Exit(1)
+	return recs
 }
 
 func main() {
 	var (
-		argc int
-		f    *os.File
-		recs map[string]string
-		v    string
-		ok   bool
+		argc    int
+		recs    [][]string
+		builder strings.Builder
+		i       int
 	)
 
 	argc = len(os.Args)
@@ -106,19 +107,30 @@ func main() {
 	switch os.Args[1] {
 	case "help":
 		help()
+	case "keys":
+		recs = readFile()
+
+		for i = range recs {
+			builder.WriteString(strings.ReplaceAll(recs[i][0], "\n", "\\n"))
+			builder.WriteByte('\n')
+		}
+
+		fmt.Print(builder.String())
 	case "copy":
-		if argc != 3 {
-			die("%s: expected 3 arguments, got %d\n", os.Args[0], argc)
+		if argc < 3 {
+			die("%s: missing <KEY> argument\n", os.Args[0])
 		}
 
-		f, recs = readFile(os.O_RDONLY)
-		closeFile(f)
-
-		v, ok = recs[os.Args[2]]
-		if !ok {
-			die("%s: key %q not found\n", os.Args[0], os.Args[2])
+		recs = readFile()
+		for i = range recs {
+			if recs[i][0] == os.Args[2] {
+				fmt.Println(recs[i][1])
+				os.Exit(0)
+			}
 		}
 
-		fmt.Println(v)
+		die("%s: key %q not found\n", os.Args[0], os.Args[2])
+	default:
+		die("%s: unknown argument %q\n", os.Args[0], os.Args[1])
 	}
 }
